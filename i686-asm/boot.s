613d99085f0557e91a646d235afe728d01945a80
Samantha Doran <samanthadoran3@gmail.com>
2016-01-13 18:00:23 -0500

    Change boot asm file so that splitting isrs and irqs in the future will be easier

--- i686-asm/boot.s
+++ i686-asm/boot.s
@@ -39,11 +39,15 @@ mboot:
 stublet:
 		extern kernel_early
 		extern kernel_main
+		extern _init
+		cli
 		call kernel_early
+		call _init
 		call kernel_main
     jmp $
 
 
+; Shortly we will add code for loading the GDT right here!
 ; This will set up our new segment registers. We need to do
 ; something special in order to set CS. We do what is called a
 ; far jump. A jump that includes a segment as well as an offset.
@@ -70,9 +74,6 @@ idt_load:
     lidt [idtp]
     ret
 
-
-; In just a few pages in this tutorial, we will add our Interrupt
-; Service Routines (ISRs) right here!
 global isr0
 global isr1
 global isr2
@@ -105,23 +106,6 @@ global isr28
 global isr29
 global isr30
 global isr31
-global irq0
-global irq1
-global irq2
-global irq3
-global irq4
-global irq5
-global irq6
-global irq7
-global irq8
-global irq9
-global irq10
-global irq11
-global irq12
-global irq13
-global irq14
-global irq15
-
 ;  0: Divide By Zero Exception
 isr0:
     cli
@@ -340,169 +324,15 @@ isr31:
     push byte 31
     jmp isr_common_stub
 
-; 32: IRQ0
-irq0:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 32
-
-    jmp irq_common_stub
-
-; 33: IRQ1
-irq1:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 33
-
-    jmp irq_common_stub
-
-; 34: IRQ2
-irq2:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 34
-
-    jmp irq_common_stub
-
-; 35: IRQ0
-irq3:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 35
-
-    jmp irq_common_stub
-
-; 36: IRQ4
-irq4:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 36
-
-    jmp irq_common_stub
-
-; 37: IRQ5
-irq5:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 37
-
-    jmp irq_common_stub
-
-; 38: IRQ6
-irq6:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 38
-
-    jmp irq_common_stub
-
-; 39: IRQ7
-irq7:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 39
-
-    jmp irq_common_stub
-
-; 40: IRQ8
-irq8:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 40
-
-    jmp irq_common_stub
-
-; 41: IRQ9
-irq9:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 41
-
-    jmp irq_common_stub
-
-; 42: IRQ10
-irq10:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 42
-
-    jmp irq_common_stub
-
-; 43: IRQ11
-irq11:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 43
-
-    jmp irq_common_stub
-
-; 44: IRQ12
-irq12:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 44
-
-    jmp irq_common_stub
-
-; 45: IRQ13
-irq13:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 45
-
-    jmp irq_common_stub
-
-; 46: IRQ14
-irq14:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 46
-
-    jmp irq_common_stub
-
-; 347: IRQ15
-irq15:
-    cli
-    push byte 0 ; No errors for IRQ
-    push byte 47
-
-    jmp irq_common_stub
 
 ; We call a C function in here. We need to let the assembler know
 ; that '_fault_handler' exists in another file
 extern fault_handler
-extern irq_handler
 
 ; This is our common ISR stub. It saves the processor state, sets
 ; up for kernel mode segments, calls the C-level fault handler,
 ; and finally restores the stack frame.
 isr_common_stub:
-    pusha
-    push ds
-    push es
-    push fs
-    push gs
-    mov ax, 0x10   ; Load the Kernel Data Segment descriptor!
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov eax, esp   ; Push us the stack
-    push eax
-    mov eax, fault_handler
-    call eax       ; A special call, preserves the 'eip' register
-    pop eax
-    pop gs
-    pop fs
-    pop es
-    pop ds
-    popa
-    add esp, 8     ; Cleans up the pushed error code and pushed ISR number
-    iret           ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP!
-
-; This is a stub that we have created for IRQ based ISRs. This calls
-; '_irq_handler' in our C code. We need to create this in an 'irq.c'
-irq_common_stub:
     pusha
     push ds
     push es
@@ -515,7 +345,7 @@ irq_common_stub:
     mov gs, ax
     mov eax, esp
     push eax
-    mov eax, irq_handler
+    mov eax, fault_handler
     call eax
     pop eax
     pop gs
@@ -525,6 +355,168 @@ irq_common_stub:
     popa
     add esp, 8
     iret
+
+global irq0
+global irq1
+global irq2
+global irq3
+global irq4
+global irq5
+global irq6
+global irq7
+global irq8
+global irq9
+global irq10
+global irq11
+global irq12
+global irq13
+global irq14
+global irq15
+
+; 32: IRQ0
+irq0:
+    cli
+    push byte 0
+    push byte 32
+    jmp irq_common_stub
+
+; 33: IRQ1
+irq1:
+    cli
+    push byte 0
+    push byte 33
+    jmp irq_common_stub
+
+; 34: IRQ2
+irq2:
+    cli
+    push byte 0
+    push byte 34
+    jmp irq_common_stub
+
+; 35: IRQ3
+irq3:
+    cli
+    push byte 0
+    push byte 35
+    jmp irq_common_stub
+
+; 36: IRQ4
+irq4:
+    cli
+    push byte 0
+    push byte 36
+    jmp irq_common_stub
+
+; 37: IRQ5
+irq5:
+    cli
+    push byte 0
+    push byte 37
+    jmp irq_common_stub
+
+; 38: IRQ6
+irq6:
+    cli
+    push byte 0
+    push byte 38
+    jmp irq_common_stub
+
+; 39: IRQ7
+irq7:
+    cli
+    push byte 0
+    push byte 39
+    jmp irq_common_stub
+
+; 40: IRQ8
+irq8:
+    cli
+    push byte 0
+    push byte 40
+    jmp irq_common_stub
+
+; 41: IRQ9
+irq9:
+    cli
+    push byte 0
+    push byte 41
+    jmp irq_common_stub
+
+; 42: IRQ10
+irq10:
+    cli
+    push byte 0
+    push byte 42
+    jmp irq_common_stub
+
+; 43: IRQ11
+irq11:
+    cli
+    push byte 0
+    push byte 43
+    jmp irq_common_stub
+
+; 44: IRQ12
+irq12:
+    cli
+    push byte 0
+    push byte 44
+    jmp irq_common_stub
+
+; 45: IRQ13
+irq13:
+    cli
+    push byte 0
+    push byte 45
+    jmp irq_common_stub
+
+; 46: IRQ14
+irq14:
+    cli
+    push byte 0
+    push byte 46
+    jmp irq_common_stub
+
+; 47: IRQ15
+irq15:
+    cli
+    push byte 0
+    push byte 47
+    jmp irq_common_stub
+
+extern irq_handler
+
+irq_common_stub:
+    pusha
+    push ds
+    push es
+    push fs
+    push gs
+
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov eax, esp
+
+    push eax
+    mov eax, irq_handler
+    call eax
+    pop eax
+
+    pop gs
+    pop fs
+    pop es
+    pop ds
+    popa
+    add esp, 8
+    iret
+
+; In just a few pages in this tutorial, we will add our Interrupt
+; Service Routines (ISRs) right here!
+
 
 
 ; Here is the definition of our BSS section. Right now, we'll use
